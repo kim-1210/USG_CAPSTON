54d1094cd37bf7ff061a667ad7a85e0c393da711
KDY <zxmzxm1324@gmail.com>
2024-03-18 18:23:28 +0900

    참고 정보를 작성하였습니다.

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 from charset_normalizer import detect
 from flask import Flask, render_template, Response, request, jsonify
+from flask_socketio import SocketIO, emit
 import sys
 import firebase_storage as fs
 import ai_cal as ai
@@ -24,5 +25,18 @@ def process_image_route():
 def go_to_work():
     return render_template('./user/check_safe_cloth.html')
 
+
 if __name__ == "__main__":
     app.run(debug = True, host="127.0.0.1", port = 8080)
+
+
+#     # 데이터베이스에 새로운 정보가 추가될 때 호출되는 함수
+# def on_new_data(event):
+#     data = event["data"]
+#     emit('new_data', data, broadcast=True)
+
+# # 데이터베이스의 변경 사항을 감지하고 on_new_data 함수를 호출하여 웹 소켓을 통해 데이터를 전송
+# def listen_to_database():
+#     my_stream = db.child("your_data_path").stream(on_new_data)
+    
+#실시간 시각화 할것 : 금일 출근
