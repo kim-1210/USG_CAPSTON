31767de3885576b237cd5cc0ded5f9c6dccf84cc
KDY <zxmzxm1324@gmail.com>
2024-03-20 11:57:51 +0900

    firebase의 function추가 했습니다

--- server.py
+++ server.py
@@ -5,8 +5,8 @@ import sys
 import firebase_storage as fs
 import ai_cal as ai
 
-
 app = Flask(__name__, static_folder='static')
+socketio = SocketIO(app)
 
 @app.route('/')
 def index():
@@ -25,18 +25,32 @@ def process_image_route():
 def go_to_work():
     return render_template('./user/check_safe_cloth.html')
 
+@app.route('/create_user', methods=['POST']) #추가
+def create_user():
+    create_information = request.json.get('information', '')
+    result_string = fs.create_user(create_information['corporation'], create_information['typed'], 
+                                   create_information['id'], create_information['password'], 
+                                   create_information['name'], create_information['birthday'])
+    my_stream = fs.db.child(create_information['corporation']).child('user').child(create_information['typed']).stream(on_new_data)
+    return jsonify({'result_alert' : result_string})
+
+@app.route('/remove_user', methods=['POST']) #삭제
+def remove_user():
+    remove_information = request.json.get('information','')
+    result_string = fs.user_remove(remove_information['corporation'], remove_information['typed'], remove_information['id'])
+    my_stream = fs.db.child(result_string['corporation']).child('user').child(result_string['typed']).stream(on_new_data)
+    return jsonify({'result_alert' : result_string})
+
+
+
+# 데이터베이스에 새로운 정보가 삭제, 추가될 때 호출되는 함수 (실시간)
+#실시간 시각화 할것 : 금일 출근
+def on_new_data(event):
+    data = event["data"]
+    print(type(data))
+    print(data)
+    # 새로운 데이터를 클라이언트에게 전송
+    emit('new_data', data, broadcast=True)
 
 if __name__ == "__main__":
-    app.run(debug = True, host="127.0.0.1", port = 8080)
-
-
-#     # 데이터베이스에 새로운 정보가 추가될 때 호출되는 함수
-# def on_new_data(event):
-#     data = event["data"]
-#     emit('new_data', data, broadcast=True)
-
-# # 데이터베이스의 변경 사항을 감지하고 on_new_data 함수를 호출하여 웹 소켓을 통해 데이터를 전송
-# def listen_to_database():
-#     my_stream = db.child("your_data_path").stream(on_new_data)
-    
-#실시간 시각화 할것 : 금일 출근
+    socketio.run(app, debug=True, host="127.0.0.1", port=8080)
