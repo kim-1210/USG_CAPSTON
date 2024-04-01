a574d5626bdfeed9ebbd804c385fd29e91b5f92f
KDY <zxmzxm1324@gmail.com>
2024-04-01 23:10:50 +0900

    슛!

--- static/js/safe_detector/photoSuggest.js
+++ static/js/safe_detector/photoSuggest.js
@@ -1,19 +1,23 @@
 let file = null;
 function loadFile(input) {
+    document.getElementById('img_upload').innerHTML = "";
     file = input.files[0]; // 선택파일 가져오기
 
     let newImage = document.createElement("img"); //새 이미지 태그 생성
-
-    //이미지 source 가져오기
-    newImage.src = URL.createObjectURL(file);
-    newImage.id = 'upload_img';
-    newImage.style.width = "100%"; //div에 꽉차게 넣으려고
-    newImage.style.height = "100%";
-    newImage.style.objectFit = "cover"; // div에 넘치지 않고 들어가게
-
-    //이미지를 image-show div에 추가
-    let container = document.getElementById('img_upload');
-    container.appendChild(newImage);
+    if (file instanceof Blob || file instanceof File) {
+        // 파일이 올바른 형식인 경우 이미지 source 가져오기
+        newImage.src = URL.createObjectURL(file);
+        newImage.id = 'upload_img';
+        newImage.style.width = "100%"; //div에 꽉차게 넣으려고
+        newImage.style.height = "100%";
+        newImage.style.objectFit = "cover"; // div에 넘치지 않고 들어가게
+    
+        //이미지를 image-show div에 추가
+        let container = document.getElementById('img_upload');
+        container.appendChild(newImage);
+    } else {
+        console.error("잘못된 파일 형식입니다.");
+    }
 }
 
 function upload(){ //건의사항 업로드
